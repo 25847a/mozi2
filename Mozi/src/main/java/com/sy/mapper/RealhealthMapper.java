e9874976e5e1bda8ca77353d6804c134749c078c
25847a <47727969+25847a@users.noreply.github.com>
2019-03-31 16:05:03 +0800

    第二次更新

---
+++ Mozi/src/main/java/com/sy/mapper/RealhealthMapper.java
@@ -0,0 +1,11 @@
+package com.sy.mapper;
+
+import com.sy.pojo.Realhealth;
+
+public interface RealhealthMapper {
+	/**
+	 * 有效真实数据--该表只为内部看，未修改的天津真实数据
+	 * @param realhealth
+	 */
+	public  void insertRealhealth(Realhealth realhealth);
+}
